f0cbe620ae2ea20201886cfe84e15dc3c82e86bf
K-SzDaniel <100788429+K-SzDaniel@users.noreply.github.com>
2022-09-28 12:52:42 +0200

    made sorter

--- src/main/java/org/example/TimeFormatter.java
+++ src/main/java/org/example/TimeFormatter.java
@@ -2,17 +2,18 @@ package org.example;
 
 import org.example.exception.NegativeNumberException;
 
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.List;
 
 public class TimeFormatter {
 
     public String formatSecondsForReadableFormat(Long seconds) {
         Long number;
+        Long test;
+        String test2;
         boolean isTheFirstString = true;
-        Map<String, Long> timeTypeInSeconds = new TreeMap<>(Map.of("year", 31536000L, "day", 86400L,
-                "hour", 3600L, "minute", 60L, "seconds", 1L));
 
+        List<Long> timeTypeInSeconds = (List.of(31536000L, 86400L, 3600L, 60L, 1L));
+        List<String> timeTypeTitle = (List.of("year", "day", "hour", "minute", " seconds"));
 
         StringBuilder result = new StringBuilder();
 
@@ -23,54 +24,27 @@ public class TimeFormatter {
             return result.append("now").toString();
         }
 
-        for (Map.Entry<String, Long> stringLongEntry : timeTypeInSeconds.entrySet()) {
+        for (int i = 0; i < 5; i++) {
+            test = timeTypeInSeconds.get(i);
+            test2 = timeTypeTitle.get(i);
 
-//            if ((seconds % stringLongEntry.getValue()) == 0) {
-//                number = Math.floorDiv(seconds, stringLongEntry.getValue());
-//                result = isTheFirstString ? result.append(number).append(" ").append(stringLongEntry.getKey()) :
-//                        result.append(", ").append(number).append(" ").append(stringLongEntry.getKey());
-//                return result.toString();
-//            }
-            if ((seconds / stringLongEntry.getValue()) >= 1) { //year
-                number = Math.floorDiv(seconds, stringLongEntry.getValue());
-                seconds -= (Math.floorDiv(seconds, stringLongEntry.getValue()) * stringLongEntry.getValue());
-                result = isTheFirstString ? result.append(number).append(" ").append(stringLongEntry.getKey()) :
-                        result.append(", ").append(number).append(" ").append(stringLongEntry.getKey());
+            if ((seconds / test) >= 1) { //year
+                number = Math.floorDiv(seconds, test);
+                seconds -= (Math.floorDiv(seconds, test) * test);
+
+                if (isTheFirstString && !test2.equals(" seconds")) {
+                    result.append(number).append(" ").append(test2);
+                } else if (test2.equals(" seconds")) {
+                    result.append(" and ").append(number).append(test2);
+                } else {
+                    result.append(", ").append(number).append(" ").append(test2);
+                }
+
                 isTheFirstString = false;
             }
-
-
         }
 
 
-//        if ((seconds / 31536000L) >= 1) { //year
-//            number = Math.floorDiv(seconds, 31536000L);
-//            seconds -= (Math.floorDiv(seconds, 31536000L) * 31536000L);
-//            result = isTheFirstString ? result.append(number).append(" year") : result.append(", ").append(number).append(" year");
-//            isTheFirstString = false;
-//        }
-//        if ((seconds / 86400L) >= 1) { //day
-//            number = Math.floorDiv(seconds, 86400L);
-//            seconds -= (Math.floorDiv(seconds, 86400L) * 86400L);
-//            result = isTheFirstString ? result.append(number).append(" day") : result.append(", ").append(number).append(" day");
-//            isTheFirstString = false;
-//        }
-//        if ((seconds / 3600L) >= 1) { //hour
-//            number = Math.floorDiv(seconds, 3600L);
-//            seconds -= (Math.floorDiv(seconds, 3600L) * 3600L);
-//
-//            result = isTheFirstString ? result.append(number).append(" hour") : result.append(", ").append(number).append(" hour");
-//            isTheFirstString = false;
-//        }
-//        if ((seconds / 60L) >= 1) { //minute
-//            number = Math.floorDiv(seconds, 60L);
-//            seconds -= (Math.floorDiv(seconds, 60L) * 60L);
-//            result = isTheFirstString ? result.append(number).append(" minute") : result.append(", ").append(number).append(" minute");
-//        }
-//        if ((seconds) >= 1 && seconds <= 60L) { //sec
-//            number = Math.floorDiv(seconds, 1L);
-//            result.append(" and ").append(number).append(" seconds");
-//        }
         return result.toString();
     }
 
